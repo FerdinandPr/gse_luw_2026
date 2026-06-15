68abdc4ffc76171bf67b4a8b5952b96d7fa470b8
Ferdinand Prahst <ferdinand.prahst@itgain.de>
2026-06-15 14:21:54 +0000

    modify loop

--- user/create_user_loop.sh
+++ user/create_user_loop.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# Skript: user_add.sh
 # Beschreibung: Legt einen Benutzer mit Passwort an (über Parameter)
-# Usage: sudo ./user_add.sh <benutzername> <passwort>
+# Usage: sudo ./create_user.sh <benutzername> <passwort>
 
-./user_add.sh user1 itgain
-./user_add.sh dbsecadm itgain
+./create_user.sh user1 itgain
+./create_user.sh dbsecadm itgain
